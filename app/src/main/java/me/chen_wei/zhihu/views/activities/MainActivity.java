55f98bbd7e264a34c15a296ea2cc4a2a087ff063
ChenWei <hander_wei@163.com>
2016-02-26 19:55:28 +0800

    Finish Main Activity UI

--- app/src/main/java/me/chen_wei/zhihu/views/activities/MainActivity.java
+++ app/src/main/java/me/chen_wei/zhihu/views/activities/MainActivity.java
@@ -1,31 +1,33 @@
 package me.chen_wei.zhihu.views.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import me.chen_wei.zhihu.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.tool_bar)
+    Toolbar toolbar;
+
+    @Bind(R.id.news_list)
+    RecyclerView mNewsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        
     }
 
 }
